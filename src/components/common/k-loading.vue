20ebe4c0fbcc7447f7d255903b1a066bb94a537a
陈锴 <2351988538@qq.com>
2021-02-03 09:15:49 +0800

    save

---
+++ src/components/common/k-loading.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="k-loading" v-if="loading">
+    <i class="el-icon-loading"></i>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    loading: {
+      type: Boolean,
+      default: true,
+    },
+  },
+};
+</script>
+<style lang="scss" scoped>
+.k-loading {
+  position: absolute;
+  height: 100%;
+  width: 100%;
+  font-size: 40px;
+  z-index: 999999;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 0.6);
+}
+</style>
--- src/components/dialogs/upload/index.vue
+++ src/components/dialogs/upload/index.vue
@@ -5,16 +5,19 @@
       v-model="dialogVisible"
       width="30%"
       :before-close="handleClose">
-      <div class="upload-wrapper upload-start" @dragenter.prevent="dragenter" @dragover.prevent="dragover"
-        @drop.prevent="drop">
-        <input ref="uploadFile" style="display:none;" type="file" id="select-file"
-          :accept="acceptTyles" multiple @change="onFileSelected">
-        <label for="select-file" class="upload-start-content"><k-icon class="zy-m-margin" icon="icon-shangchuan" :size="24"></k-icon>拖拽文件至此处或点击上传文件</label>
+      <div class="upload-wrapper">
+        <KLoading :loading="isHandleSelectFile"></KLoading>
+        <div v-if="curentStep === steps.startUpload.value" class="upload-start" @dragenter.prevent="dragenter" @dragover.prevent="dragover"
+          @drop.prevent="drop">
+          <input ref="uploadFile" style="display:none;" type="file" id="select-file"
+            :accept="acceptTyles" multiple @change="onFileSelected">
+          <label for="select-file" class="upload-start-content"><k-icon class="zy-m-margin" icon="icon-shangchuan" :size="24"></k-icon>拖拽文件至此处或点击上传文件</label>
+        </div>
       </div>
       <template #footer>
-        <span>
-          <el-button @click="dialogVisible = false">取 消</el-button>
-          <el-button type="primary" @click="dialogVisible = false">确 定</el-button>
+        <span v-if="curentStep === steps.startUpload.value">
+          <el-button @click="handleClose">取 消</el-button>
+          <el-button type="primary" @click="handerNext">确 定</el-button>
         </span>
       </template>
     </el-dialog>
@@ -22,22 +25,32 @@
 </template>
 
 <script>
+import KLoading from '@/components/common/k-loading';
 
-const acceptTyles = ['geojson', 'json', 'csv', 'xls', 'xlsx', 'zip'];
+const acceptTyles = ['.geojson', '.json', '.csv', '.xls', '.xlsx', '.zip'];
 const steps = {
-
+  startUpload: {
+    value: 'startUpload',
+  },
+  selectFiels: {
+    value: 'selectFiels',
+  },
 };
 export default {
+  components: {
+    KLoading,
+  },
   setup() {
     return {
-      acceptTyles,
+      acceptTyles: acceptTyles.join(','),
       steps,
     };
   },
   data() {
     return {
+      curentStep: steps.startUpload.value,
       dialogVisible: false,
-      isHandleSelectFile: true,
+      isHandleSelectFile: false,
     };
   },
   created() {
@@ -47,6 +60,18 @@ export default {
     });
   },
   methods: {
+    handleClose() {
+      this.dialogVisible = false;
+    },
+    handerNext() {
+      switch (this.curentStep) {
+        case steps.startUpload.value:
+
+          break;
+        default:
+          break;
+      }
+    },
     dragenter(e) {
       e.stopPropagation();
       e.preventDefault();
@@ -144,26 +169,28 @@ export default {
 <style lang="scss">
 .upload-wrapper {
   position: relative;
-  #select-file {
-    position: absolute;
-    width: 100%;
-    height: 100%;
-  }
-}
-.upload-start {
-  .upload-start-content {
-    height: 300px;
-    width: 100%;
-    background-color: $light-gray;
-    display: flex;
-    justify-content: center;
-    align-items: center;
-    cursor: pointer;
-    &:hover {
-      .k-icon {
-        color: $primary;
+
+  .upload-start {
+      #select-file {
+      position: absolute;
+      width: 100%;
+      height: 100%;
+    }
+    .upload-start-content {
+      height: 300px;
+      width: 100%;
+      background-color: $light-gray;
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      cursor: pointer;
+      &:hover {
+        .k-icon {
+          color: $primary;
+        }
       }
     }
   }
 }
+
 </style>
