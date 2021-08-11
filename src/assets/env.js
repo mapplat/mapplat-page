82ac5baf89e1281b36274684be95dc81eb88e800
chenkai0520 <k_0520@qq.com>
2021-08-11 17:36:10 +0800

    fix: update prod host

--- src/assets/env.js
+++ src/assets/env.js
@@ -1,4 +1,4 @@
-const SERVER_HOST = process.env.NODE_ENV === 'production' ? `://${window.location.host}` : 'http://117.50.82.140';
+const SERVER_HOST = process.env.NODE_ENV === 'production' ? window.location.origin : 'http://117.50.82.140';
 
 export default {
   SERVER_HOST,
